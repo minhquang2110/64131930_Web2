b786b98433ff79381e5089989235e2056ccabc47
Nguyễn Minh Quang <quang.nm.64cntt@ntu.edu.vn>
2025-02-24 13:42:45 +0700

    thay đổi nội dung của sum.js và làm ví dụ 1 và hai bài jsp

---
+++ HelloJSP/src/main/webapp/HelloExpression.jsp
@@ -0,0 +1,18 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@page import="java.util.Date" %>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<center>
+		<h1>Hello Tiil Edu</h1>
+		<%= new Date().toString() %>
+		<%=2+5 %>
+		<%="<br><b>Hôm nay tôi học JSP</b>" %>
+	</center>
+</body>
+</html>
--- ViduTinhToan/src/main/webapp/sum.js
+++ ViduTinhToan/src/main/webapp/sum.js
@@ -1,12 +1,13 @@
-var a=document.querySelector(".annouce");
+var a = document.querySelector(".annouce");
 function getCookie(name) {
-    let cookieArr = document.cookie.split(';');  
-    for (let i = 0; i < cookieArr.length; i++) {
-        let cookie = cookieArr[i].trim();  
-        if (cookie.indexOf(name + '=') === 0) {
-            return cookie.substring(name.length + 1); 
-        }
+  let cookieArr = document.cookie.split(";");
+  for (let i = 0; i < cookieArr.length; i++) {
+    let cookie = cookieArr[i].trim();
+    if (cookie.indexOf(name + "=") === 0) {
+      let cookieValue = cookie.substring(name.length + 1);
+      return cookieValue.replace(/_/g, " ");
     }
-    return null; 
+  }
+  return null;
 }
-a.innerText=getCookie(("annouce"))
+a.innerText = getCookie("annouce");
