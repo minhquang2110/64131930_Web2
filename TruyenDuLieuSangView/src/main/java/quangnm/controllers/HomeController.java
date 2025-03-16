a3569b01c02f13c3a0b5f60533c06c19a71f2c73
Nguyễn Minh Quang <quang.nm.64cntt@ntu.edu.vn>
2025-03-16 20:15:14 +0700

    hoàn thành controller post và get của bài Login

--- TruyenDuLieuSangView/src/main/java/quangnm/controllers/HomeController.java
+++ TruyenDuLieuSangView/src/main/java/quangnm/controllers/HomeController.java
@@ -1,7 +1,11 @@
 package quangnm.controllers;
+import java.lang.ProcessBuilder.Redirect;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import quangnm.models.Personal;
@@ -14,6 +18,40 @@ public class HomeController{
 		Personal personal=new Personal("64131930", "Nguyễn Minh Quang", 2004, true);
 		model.addAttribute("personal",personal);
 		return "index";
+	}
+	@GetMapping("truyenDsObject")
+	public String transportData(Model model) {
+		ArrayList<Personal> dsSinhVien=new ArrayList<Personal>();
+		dsSinhVien.add(new Personal("64131930", "Nguyễn Minh Quang", 2004, true));
+		dsSinhVien.add(new Personal("64131931", "Trần Thị Lan", 2003, false));
+		dsSinhVien.add(new Personal("64131932", "Phạm Văn Hùng", 2005, true));
+		dsSinhVien.add(new Personal("64131933", "Lê Minh Tuấn", 2004, true));
+		dsSinhVien.add(new Personal("64131934", "Hoàng Thị Mai", 2003, false));
+		dsSinhVien.add(new Personal("64131935", "Đoàn Ngọc Sơn", 2002, true));
+		dsSinhVien.add(new Personal("64131936", "Vũ Thị Thanh", 2005, true));
+		dsSinhVien.add(new Personal("64131937", "Ngô Minh Tiến", 2004, false));
+		dsSinhVien.add(new Personal("64131938", "Bùi Thị Nhung", 2003, true));
+		dsSinhVien.add(new Personal("64131939", "Dương Minh Hoàng", 2005, false));
+		model.addAttribute("dsSV",dsSinhVien);
+		return "transport";
+	}
+	@PostMapping("/profile")
+	public String profilePost(@RequestParam("email") String email,@RequestParam("password") String password,Model model) {
+		if(email.equals("minhquang")&&password.equals("12345")) {
+			return("profilePage");
+		}
+		return "loginPage";
+	}
+	@GetMapping("/profile")
+	public String profileGet(@RequestParam("email") String email,@RequestParam("password") String password,Model model) {
+		if(email.equals("minhquang")&&password.equals("12345")) {
+			return("profilePage");
+		}
+		return("profilePage");
+	}
+	@GetMapping("/login")
+	public String loginHandler() {
+		return "loginPage";
 	}
 	
 }
