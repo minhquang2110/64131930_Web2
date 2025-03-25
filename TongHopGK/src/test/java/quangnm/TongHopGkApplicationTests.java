5f96eabd79927d96a0a376e7fac9ba8e973312c0
Nguyễn Minh Quang <quang.nm.64cntt@ntu.edu.vn>
2025-03-25 09:20:24 +0700

    first commit

---
+++ TongHopGK/src/test/java/quangnm/TongHopGkApplicationTests.java
@@ -0,0 +1,13 @@
+package quangnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TongHopGkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
