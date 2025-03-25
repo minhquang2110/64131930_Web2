57e72fb7ec807c8d762d9dc7675633a765f4fdf8
Nguyễn Minh Quang <quang.nm.64cntt@ntu.edu.vn>
2025-03-25 09:06:20 +0700

    nộp lại bài layout

---
+++ ToChucLayout/src/test/java/quangnm/ToChucLayoutApplicationTests.java
@@ -0,0 +1,13 @@
+package quangnm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToChucLayoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
