7d2a8fc92ad73941717b8941ec5c88f6fa175969
Nguyễn Minh Quang <quang.nm.64cntt@ntu.edu.vn>
2025-04-01 09:23:55 +0700

    hoàn thành lay out và hai class

---
+++ nguyenminhquang_dashboard/src/main/java/thiGK/ntu64131930/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package thiGK.ntu64131930.controllers;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController{
+	@GetMapping("/")
+	public String home() {
+		return "layout";
+	}
+}
